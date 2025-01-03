36df4dd77ae51e06624a4f7c91f0bd46c9617384
azizsige <kurniawan00azizsige@gmail.com>
2025-01-03 23:05:04 +0700

    CMSCO-10-BE-Setup-Database

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const config = require("./config");
+
+// Routes
+// const authRoutes = require("./routes/authRoutes");
+// const roleRoutes = require("./routes/roleRoutes");
+// const userRoutes = require("./routes/userRoute");
+
+app.use(express.json());
+app.use(cors());
+// app.use(
+//   session({
+//     secret: "loremipsum",
+//     resave: true,
+//     saveUnitialized: true,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24 * 7,
+//     },
+//   })
+// );
+
+// URL Routes
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api", roleRoutes);
+// app.use("/api", userRoutes);
+
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    bufferCommands: false,
+    serverSelectionTimeoutMS: 50000,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to DB", error);
+  });
+
+module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true, // Automatically manage `createdAt` and `updatedAt`
+  }
+);
+
+// Middleware to hash password before saving the user
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// Method to compare entered password with hashed password
+// userSchema.methods.comparePassword = async function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
